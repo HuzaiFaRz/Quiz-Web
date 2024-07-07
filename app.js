efc84fc9760f00dabfb3d4f0eb6e1bf843b7df2a
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-07-06 23:06:00 -0700

    Some

---
+++ app.js
@@ -0,0 +1,84 @@
+const signUpForm = document.querySelector(".signup-form");
+let signUpUserIsLogged = false;
+
+const signInForm = document.querySelector(".signin-form");
+let signInUserIsLogged = false;
+
+const signUpFormFunctionality = () => {
+  signUpForm.addEventListener("submit", (a) => {
+    a.preventDefault();
+    const signUpFormData = new FormData(signUpForm);
+    let signUpUserInfo = {
+      name: signUpFormData.get("name"),
+      email: signUpFormData.get("signupemail"),
+      password: signUpFormData.get("signuppassword"),
+      confirmPassword: signUpFormData.get("signupconfirmpassword"),
+    };
+
+    if (
+      !signUpUserInfo.name ||
+      !signUpUserInfo.email ||
+      !signUpUserInfo.password ||
+      !signUpUserInfo.confirmPassword
+    ) {
+      console.log("Fill All Field ❌");
+    } else if (signUpUserInfo.password.length <= 8) {
+      console.log("Passowrd At Least Eight Character ❌");
+    } else if (signUpUserInfo.password !== signUpUserInfo.confirmPassword) {
+      console.log("Passowrd Does Not Match ❌");
+    }
+
+
+let new=JSON.parse(localStorage.getItem("signUpUserInfo"))
+let new2=new
+
+   
+    if (matchingUser) {
+        
+      console.log("User Already Exist Sign In");
+    } else {
+      localStorage.setItem(
+        "signUpUserInfo",
+        JSON.stringify(signUpUserInfoSave)
+      );
+      console.log("Sign Up SuccessFully");
+    }
+
+    //   signUpForm.reset();
+  });
+
+  signInForm.addEventListener("submit", (b) => {
+    b.preventDefault();
+
+    const signInFormData = new FormData(signInForm);
+    if (
+      !signInFormData.get("signinemail") ||
+      !signInFormData.get("signinpassword")
+    ) {
+      console.log("Fill All Field ❌");
+    } else {
+      let storedUsers =
+        JSON.parse(localStorage.getItem("signUpUserInfo")) || [];
+      let matchingUser = storedUsers.find(
+        (user) => user.email === signInFormData.get("signinemail"),
+        (user) => user.password === signInFormData.get("signinpassword")
+      );
+
+      if (matchingUser) {
+        signInUserIsLogged = true;
+        if (signInUserIsLogged) {
+          signInForm.reset();
+          console.log("Sign in SuccessFully");
+        }
+      } else {
+        console.log("User Not Found Sign Up ❌");
+      }
+    }
+
+    // let signInUserInfo = {
+    //   email: signInFormData.get("signinemail"),
+    //   password: signInFormData.get("signinpassword"),
+    // };
+  });
+};
+signUpFormFunctionality();
